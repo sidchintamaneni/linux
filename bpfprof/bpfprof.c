afbfd0e2bb889b1b505a08ae06c807aa1274a61a
Siddharth Chintamaneni <sidchintamaneni@gmail.com>
2025-09-28 22:12:35 +0000

    bpfprof: added a dummy tracing call - it got opt anyway

--- bpfprof/bpfprof.c
+++ bpfprof/bpfprof.c
@@ -3,7 +3,12 @@
 #include <linux/filter.h>
 #include <linux/syscalls.h>
 
+static noinline void dummy_trace_call(struct pt_regs *regs) {
+	return;
+}
+
 SYSCALL_DEFINE0(bpfprof)
 {
+	dummy_trace_call(NULL);
 	return 0;
 }
